dd1c08bdb8adda0c73b9b0a044eab04253725014
Skyler Link <slink@vargomail.com>
2024-03-21 08:29:52 -0400

    Initial commit

---
+++ src/main/java/com/skylerlink/interviewapp/InterviewAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skylerlink.interviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skylerlink/interviewapp/InterviewAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skylerlink.interviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
