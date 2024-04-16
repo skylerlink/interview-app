2a024ed134bbc1b2b7ce6715efb076ebc53ffdff
Skyler Link <slink@vargomail.com>
2024-04-16 10:58:07 -0400

    Built REST API to get ALL employees

--- src/main/java/com/skylerlink/interviewapp/service/EmployeeService.java
+++ src/main/java/com/skylerlink/interviewapp/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.skylerlink.interviewapp.service;
 
 import com.skylerlink.interviewapp.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/com/skylerlink/interviewapp/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/skylerlink/interviewapp/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.skylerlink.interviewapp.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -30,5 +33,12 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .orElseThrow(() -> new ResourceNotFoundException("Could not find an employee with id " + employeeId));
 
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
