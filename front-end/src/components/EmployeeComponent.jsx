abeb15dd6ae9ccabdf068804e68c33c4b051ff7c
Skyler Link <slink@vargomail.com>
2024-04-18 09:27:44 -0400

    Created update employee http route

--- front-end/src/components/EmployeeComponent.jsx
+++ front-end/src/components/EmployeeComponent.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {createEmployee, getEmployee} from '../services/EmployeeService'
+import {createEmployee, getEmployee, updateEmployee} from '../services/EmployeeService'
 import {useNavigate, useParams} from 'react-router-dom'
 
 const EmployeeComponent = () => {
@@ -30,17 +30,29 @@ const EmployeeComponent = () => {
         }
     }, [id])
 
-    function saveEmployee(e){
+    function saveOrUpdateEmployee(e){
         e.preventDefault();
 
         if(validateForm()){
+
             const employee = {firstName, lastName, email}
             console.log(employee)
 
-            createEmployee(employee).then((response) => {
-                console.log(response.data);
-                navigator('/employees')
-            })
+            if(id){
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
         }
     }
 
@@ -136,7 +148,7 @@ const EmployeeComponent = () => {
                             {errors.email && <div className='invalid-feedback'> {errors.email} </div>}
                         </div>
 
-                        <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
                         
                     </form>
                 </div>
