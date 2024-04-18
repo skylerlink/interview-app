51cca4c1013cfaa9be0586ddce70748ee212efeb
Skyler Link <slink@vargomail.com>
2024-04-18 08:47:31 -0400

    Added update button, dynamically named the add/update form

--- front-end/src/components/EmployeeComponent.jsx
+++ front-end/src/components/EmployeeComponent.jsx
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import {createEmployee} from '../services/EmployeeService'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 
 const EmployeeComponent = () => {
 
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+
+    const {id} = useParams();
 
     const [errors, setErrors] = useState({
         firstName: '',
@@ -62,12 +64,22 @@ const EmployeeComponent = () => {
 
     }
 
+    function pageTitle(){
+        if(id){
+            return <h2 className='text-center'>Update Employee</h2>
+        } else{
+            return <h2 className='text-center'>Add Employee</h2>
+        }
+    }
+
   return (
     <div className='container'>
         <br /><br />
         <div className='row'>
             <div className='card col-md-6 offset-md-3 offset-md-3'>
-                <h2 className='text-center'>Add Employee</h2>
+                {
+                    pageTitle()
+                }
                 <div className='card-body'>
                     <form>
                         <div className='form-group mb-2'>
